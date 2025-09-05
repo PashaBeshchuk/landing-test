eebdbd9e0ab6e1f45dca9c878e8f7828e3f4bf35
Pavlo_Beshchuk <rocaroca@MacBook-Pro-Roca.local>
2025-09-05 20:59:33 +0300

    Fix path server 2

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/api/temperature', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
